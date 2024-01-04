80be62597d7c00c363a1fbf60c8b020617a18a75
diazramzy <diazramzyy@gmail.com>
2024-01-04 22:39:08 +0700

    Edit Action Logout Button SiswaHomeView

--- src/main/java/com/mycompany/kuisapp/view/SiswaHomeView.java
+++ src/main/java/com/mycompany/kuisapp/view/SiswaHomeView.java
@@ -328,8 +328,10 @@ public class SiswaHomeView extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        
+        SiswaLoginView siswaLoginView = new SiswaLoginView();
+        siswaLoginView.setLocationRelativeTo(null);
+        siswaLoginView.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
